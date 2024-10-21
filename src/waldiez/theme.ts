2472baa2aceaa2b9573f84c113772e063f9907e7
Lazaros Toumanidis <laztoum@protonmail.com>
2024-10-21 19:45:51 +0300

    update agent colors

--- src/waldiez/theme.ts
+++ src/waldiez/theme.ts
@@ -26,9 +26,9 @@ export const LOGOS = {
 };
 export const AGENT_COLORS = {
   agent: '#005490',
-  assistant: '#8ed973',
-  user: '#d86ecc',
-  manager: '#e97132',
+  assistant: '#66bc57',
+  user: '#bc76f5',
+  manager: '#ed8a34',
   rag_user: '#e34561'
 };
 export const AGENT_ICONS = {
