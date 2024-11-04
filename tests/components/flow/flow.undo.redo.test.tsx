063073bba360432649e1e12133a9b5ad2b9727b8
Lazaros Toumanidis <laztoum@protonmail.com>
2024-11-04 11:16:45 +0200

    add un/re do tests

--- tests/components/flow/flow.undo.redo.test.tsx
+++ tests/components/flow/flow.undo.redo.test.tsx
@@ -1,26 +1,25 @@
-import { renderFlow} from './common';
+import { renderFlow } from './common';
 import { act, fireEvent, screen } from '@testing-library/react';
+import userEvent, { UserEvent } from '@testing-library/user-event';
 import { describe, it } from 'vitest';
 
-import userEvent, { UserEvent } from '@testing-library/user-event'
-
 const undoAction = async (user: UserEvent) => {
-    act(() => {
-        renderFlow();
-    });
-    fireEvent.click(screen.getByTestId('show-agents'));
-    const agentFooter = screen.getByTestId('agent-footer-agent-0');
-    expect(agentFooter).toBeTruthy();
-    const cloneDiv = agentFooter.querySelector('.clone-agent');
-    expect(cloneDiv).toBeTruthy();
-    fireEvent.click(cloneDiv as HTMLElement);
-    vi.advanceTimersByTime(50);
-    const clonedAgentView = screen.queryByText('Node 0 (copy)');
-    expect(clonedAgentView).toBeTruthy();
-    await user.keyboard('{Control>}z{/Control}');
-    vi.advanceTimersByTime(50);
-    const clonedAgentViewAfterUndo = screen.queryByText('Node 0 (copy)');
-    expect(clonedAgentViewAfterUndo).toBeNull();
+  act(() => {
+    renderFlow();
+  });
+  fireEvent.click(screen.getByTestId('show-agents'));
+  const agentFooter = screen.getByTestId('agent-footer-agent-0');
+  expect(agentFooter).toBeTruthy();
+  const cloneDiv = agentFooter.querySelector('.clone-agent');
+  expect(cloneDiv).toBeTruthy();
+  fireEvent.click(cloneDiv as HTMLElement);
+  vi.advanceTimersByTime(50);
+  const clonedAgentView = screen.queryByText('Node 0 (copy)');
+  expect(clonedAgentView).toBeTruthy();
+  await user.keyboard('{Control>}z{/Control}');
+  vi.advanceTimersByTime(50);
+  const clonedAgentViewAfterUndo = screen.queryByText('Node 0 (copy)');
+  expect(clonedAgentViewAfterUndo).toBeNull();
 };
 
 describe('Flow Undo Redo', () => {
