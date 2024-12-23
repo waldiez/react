4599a1071dc88170f0f16cb5e5e5ac381a035bb7
Lazaros Toumanidis <laztoum@protonmail.com>
2024-12-23 13:37:18 +0200

    update output paths

--- vite.config.ts
+++ vite.config.ts
@@ -21,6 +21,7 @@ export default defineConfig(({ command }) => ({
     publicDir: command === "build" ? resolve(__dirname, "public", "logo") : resolve(__dirname, "public"),
     build: {
         emptyOutDir: true,
+        outDir: resolve(__dirname, "dist", "lib"),
         lib: {
             entry: resolve(__dirname, "src", "waldiez", "index.ts"),
             name: "@waldiez",
